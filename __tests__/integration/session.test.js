1f8ced28baa8d296a8e4bed1049ba72b8db81076
rayonnunes <rayon.nunes@redeinova.net>
2021-03-28 16:11:27 -0300

    Refatoração para adequação ao teste

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,26 +1,25 @@
 const request = require('supertest')
+
 const app = require('../../src/app')
 const { User } = require('../../src/app/models')
-
-describe('signUp', () => {
-  it('should create a user', async() => {
-    expect(user.email).toBe('rayonnunes@hotmail.com')
-  })
-})
+const truncateTables = require('../utils/truncateTables')
 
 describe('Authentication', () => {
+  beforeEach(async() => {
+    await truncateTables()
+  })
   it('should authenticate with valid cretentials', async () => {
     const user = await User.create({
       name: 'Rayon',
       email: 'rayonnunes@hotmail.com',
-      password: '123456'
+      password_hash: '123456'
     })
     const response = await request(app)
       .post('/session')
       .send({
         name: 'Rayon',
         email: 'rayonnunes@hotmail.com',
-        password: '123456'
+        password_hash: '123456'
       })
     expect(response.status).toBe(200)
   })
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
 const routes = require('express').Router()
+const SessionController = require('./app/controller/SessionController')
+routes.post('/session',  SessionController.store)
 
 module.exports = routes
